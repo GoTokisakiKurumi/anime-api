c484f773dc627cddc12dfb440a92a6e8a98e3bcb
lutfi aulia sidik <biolutfi@gmail.com>
2023-10-09 19:04:04 +0700

    ok

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import animeRoutes from './routes/animeRoutes.js'
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Izinkan domain ini
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Metode HTTP yang diizinkan
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Header tambahan yang diizinkan
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Izinkan pengiriman kredensial (seperti cookies)
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Izinkan domain ini
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Metode HTTP yang diizinkan
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Header tambahan yang diizinkan
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Izinkan pengiriman kredensial (seperti cookies)
 
-  next();
-});
+//   next();
+// });
 app.use(animeRoutes)
 app.use(cors())
 
