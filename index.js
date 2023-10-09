4b5d464804929156386ac8b34aa38a599719d4fc
lutfi aulia sidik <biolutfi@gmail.com>
2023-10-09 19:05:09 +0700

    ok

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ import animeRoutes from './routes/animeRoutes.js'
 
 const app = express()
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Izinkan domain ini
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Metode HTTP yang diizinkan
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Header tambahan yang diizinkan
-//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Izinkan pengiriman kredensial (seperti cookies)
-
-//   next();
-// });
 app.use(animeRoutes)
 app.use(cors())
 
