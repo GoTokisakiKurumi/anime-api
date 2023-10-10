88ce7ab30df6e2561a99a8c465d1001253d6631d
lutfi aulia sidik <biolutfi@gmail.com>
2023-10-10 14:43:53 +0700

    ok

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const animeRoutes = require('./routes/animeRoutes.js');
-
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
 
 app.use(animeRoutes);
 
