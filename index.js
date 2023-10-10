cecf9a45d4c1f2612ebbb5d9be4b53fb3ffa0bd0
lutfi aulia sidik <biolutfi@gmail.com>
2023-10-10 14:18:11 +0700

    ok

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import express from 'express';
-import cors from 'cors'
-import animeRoutes from './routes/animeRoutes.js'
-
-const app = express()
-
-app.use(animeRoutes)
-app.use(cors())
-
-app.listen(3000, () => console.log('Anime API on ready 🚀'))
 
