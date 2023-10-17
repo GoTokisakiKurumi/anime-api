de16a04557d869b84905dcd5e76f342a1a2ec039
lutfi aulia sidik <biolutfi@gmail.com>
2023-10-17 10:06:43 +0700

    ok

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://pustaka-anime.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
